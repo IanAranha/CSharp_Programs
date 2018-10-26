343c891d056a1e656e993df4923c52a412bf7b67
Ian Aranha <ianorama@gmail.com>
2018-10-26 15:54:16 -0700

    Procedural programming Exercise5 files added.

---
+++ ProceduralProgramming5/ProceduralProgramming5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProceduralProgramming5
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Exercise5();
+        }
+
+        public static void Exercise5()
+        {
+            Console.WriteLine("Enter a word: ");
+            var input = Console.ReadLine();
+            var message = CountTheVowels(input);
+            Console.WriteLine(message);
+        }
+
+        public static string CountTheVowels(String input)
+        {
+            var str = input.ToLower();  
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                var message = "Invalid String";
+                return message;
+            }
+
+            var vowels = new List<char>(new char[] { 'a', 'e', 'i', 'o', 'u' });
+
+            var vowelCounter = 0;
+
+            foreach (var character in str)
+                if (vowels.Contains(character))
+                    vowelCounter += 1;
+            return "Total number of vowels: " + vowelCounter;
+        }
+    }
+}
